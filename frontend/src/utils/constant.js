ebc47f254705a69fbfa463f38319886c421e6636
praveensarraf <praveen96650@gmail.com>
2025-09-27 21:58:06 +0530

    API updated

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,9 +1,5 @@
-export const USER_API_END_POINT = "https://findyourjobb.onrender.com/api/v1/user";
-
-export const JOB_API_END_POINT = "https://findyourjobb.onrender.com/api/v1/job";
-
-export const APPLICATION_API_END_POINT = "https://findyourjobb.onrender.com/api/v1/application";
-
-export const COMPANY_API_END_POINT = "https://findyourjobb.onrender.com/api/v1/company";
-
-export const EMAIL_API_END_POINT = "https://findyourjobb.onrender.com/api/v1/email";
+export const USER_API_END_POINT = import.meta.env.VITE_USER_API_END_POINT;
+export const JOB_API_END_POINT = import.meta.env.VITE_JOB_API_END_POINT;
+export const APPLICATION_API_END_POINT = import.meta.env.VITE_APPLICATION_API_END_POINT;
+export const COMPANY_API_END_POINT = import.meta.env.VITE_COMPANY_API_END_POINT;
+export const EMAIL_API_END_POINT = import.meta.env.VITE_EMAIL_API_END_POINT;
